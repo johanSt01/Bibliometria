5339f2b8b23d65a5b1cc6d7168a842f986817ae9
Johan Stiven <johans.londonoa1@uqvirtual.edu.co>
2024-11-07 21:25:32 -0500

    config

--- server.py
+++ server.py
@@ -101,5 +101,6 @@ def index():
     )
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    debug_mode = os.environ.get("DEBUG", "False") == "True"
+    app.run(host="0.0.0.0", port=port, debug=debug_mode)
